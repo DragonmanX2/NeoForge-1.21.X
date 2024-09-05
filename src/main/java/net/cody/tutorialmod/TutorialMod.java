dd80b5842fda75eeb806abf95a11aa7d0ea47735
DragonmanX2 <cody@usflying.com>
2024-09-04 17:54:55 -0700

    Creative Mode Tabs

--- src/main/java/net/cody/tutorialmod/TutorialMod.java
+++ src/main/java/net/cody/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.cody.tutorialmod;
 
 import net.cody.tutorialmod.block.ModBlocks;
+import net.cody.tutorialmod.item.ModCreativeModeTabs;
 import net.cody.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -53,6 +54,9 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
+
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
