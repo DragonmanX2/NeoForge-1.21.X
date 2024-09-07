de5611ac1fc0c20a6c424ea1dee58c827f28278b
DragonmanX2 <cody@usflying.com>
2024-09-06 21:24:59 -0700

    Added Chisel

--- src/main/java/net/dragonman/tutorialmod/TutorialMod.java
+++ src/main/java/net/dragonman/tutorialmod/TutorialMod.java
@@ -56,14 +56,13 @@ public class TutorialMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            //event.accept(ModItems.BISMUTH);
-            //event.accept(ModItems.RAW_BISMUTH);
+
+
 
         }
 
         if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            //event.accept(ModBlocks.BISMUTH_BLOCK);
-            //event.accept(ModBlocks.BISMUTH_ORE);
+
         }
 
 
--- src/main/java/net/dragonman/tutorialmod/item/ModItems.java
+++ src/main/java/net/dragonman/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.dragonman.tutorialmod.item;
 
 import net.dragonman.tutorialmod.TutorialMod;
+import net.dragonman.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -11,9 +12,13 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
     public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
         ()->new Item(new Item.Properties()));
+
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
 
+
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
 
    public static void register(IEventBus eventBus){
