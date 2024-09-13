5b4ad8c10e8c57abc440759b1ba27a8be3bc8346
DragonmanX2 <cody@usflying.com>
2024-09-13 16:25:19 -0700

    Custom Tooltips

--- src/main/java/net/dragonman/tutorialmod/item/ModItems.java
+++ src/main/java/net/dragonman/tutorialmod/item/ModItems.java
@@ -4,12 +4,17 @@ import net.dragonman.tutorialmod.TutorialMod;
 import net.dragonman.tutorialmod.item.custom.ChiselItem;
 import net.dragonman.tutorialmod.item.custom.FuelItem;
 import net.dragonman.tutorialmod.item.custom.ModFoodProperties;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-    public class ModItems {
+import java.util.List;
+
+public class ModItems {
         public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
 
     public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
@@ -22,16 +27,18 @@ import net.neoforged.neoforge.registries.DeferredRegister;
             () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static final DeferredItem <Item> RADISH = ITEMS.register("radish",
-            ()-> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+            ()-> new Item(new Item.Properties().food(ModFoodProperties.RADISH)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.dragonmanmod.radish.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
         public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
                 ()-> new FuelItem(new Item.Properties(), 800));
         public static final DeferredItem<Item> STARLIGHT_ASHES = ITEMS.register("starlight_ashes",
                 ()-> new Item(new Item.Properties()));
-
-
-
-
 
 
 
